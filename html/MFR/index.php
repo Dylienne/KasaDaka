e1f61a59fb09c35608c42586b246cc0bad7c944a
kasadaka <kasadaka@kasadaka.local>
2016-05-15 10:57:52 +0200

    good record names

--- html/MFR/index.php
+++ html/MFR/index.php
@@ -20,6 +20,8 @@ switch($_FILES['message']['error']){
 	$language = $_REQUEST['language'];
 	fwrite($fp, "\nlanguage: ".$language);
 
+	$choice = $_REQUEST['choice_user'];
+
 	fwrite($fp, "\nwhat is this: ".$_FILES['message']['tmp_name']);
         $success_path=file_exists($_FILES['message']['tmp_name']);
 	fwrite($fp, "\ncheck path origin: ".$success_path);
@@ -29,7 +31,7 @@ switch($_FILES['message']['error']){
 	fwrite($fp, "\ncheck path destiny: ".$success_path_d);
 	fwrite($fp, "\ncheck file name: ".$_FILES['message']['name']);
 	
-	$success=move_uploaded_file($_FILES['message']['tmp_name'], $new_path . $callerid . '_' . date("Y_m_d_h_i_sa") . '_' . $language . '.WAV');
+	$success=move_uploaded_file($_FILES['message']['tmp_name'], $new_path . $callerid . '_' . $choice . '_' . $language . '_' . date("Y_m_d_h_i_sa") . '.WAV');
 
 	fwrite($fp, "\ncopy success: ".$success);
 	$prompt = 'Thanks, your message has been saved.';
