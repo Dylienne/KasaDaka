cfcf524d784b79ac862baa216751369e82526c00
kasadaka <kasadaka@kasadaka.local>
2016-05-23 23:23:52 +0200

    save work with the radio

--- html/MFR/index.php
+++ html/MFR/index.php
@@ -25,13 +25,13 @@ switch($_FILES['message']['error']){
 	fwrite($fp, "\nwhat is this: ".$_FILES['message']['tmp_name']);
         $success_path=file_exists($_FILES['message']['tmp_name']);
 	fwrite($fp, "\ncheck path origin: ".$success_path);
-	$new_path=__DIR__.'/../../recordings/';
+	$new_path=__DIR__.'/../recordings/';
 	fwrite($fp, "\nnew path".$new_path);
 	$success_path_d=file_exists($new_path);
 	fwrite($fp, "\ncheck path destiny: ".$success_path_d);
 	fwrite($fp, "\ncheck file name: ".$_FILES['message']['name']);
 	
-	$success=move_uploaded_file($_FILES['message']['tmp_name'], $new_path . $callerid . '_' . $choice . '_' . $language . '_' . date("Y_m_d_h_i_sa") . '.WAV');
+	$success=move_uploaded_file($_FILES['message']['tmp_name'], $new_path . $callerid . '_' . $choice . '_' . $language . '_' . date("Y_m_d_h_i_sa") . '.wav');
 
 	fwrite($fp, "\ncopy success: ".$success);
 	$prompt = 'Thanks, your message has been saved.';
---
+++ html/MFR_Radio/index.php
@@ -0,0 +1,51 @@
+<?php
+
+if(file_exists('/tmp/request.log')){
+	unlink('/tmp/request.log');
+}
+$fp = fopen('/tmp/request.log', 'a');
+
+$language = $_REQUEST['language'];
+fwrite($fp, "\nlanguage: $language");
+
+$choice = $_REQUEST['choice_user'];
+fwrite($fp, "\nchoice: $choice");	
+
+$path=__DIR__.'/../recordings';
+
+$array=glob("$path/*\_$choice\_$language\_*.wav");
+
+if(empty($array)){
+	fwrite($fp, "\nbig problem array empty"); 
+}
+
+$arrayPaths="[";
+
+foreach($array as $filepath){
+	$subFilePath = substr($filepath, strlen(__DIR__)+1);	
+	$arrayPaths = $arrayPaths."'".$subFilePath."'".",";
+	fwrite($fp, "\npath: $subFilePath"); 
+}
+
+$arrayPaths=substr($arrayPaths, 0, -1);
+$arrayPaths=$arrayPaths."]";
+
+fwrite($fp, "\nwhatever was a success: $arrayPaths");
+fwrite($fp,'\n');
+fclose($fp);
+
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+?>
+
+<vxml version = "2.1" >
+<form id="fetch">	
+	<block>
+		<foreach item="path" array="<?php echo $arrayPaths ?>" >
+			<log expr="'*****path=' + path + '*****'" />			
+			<prompt>			
+				<audio expr="path" />
+			</prompt>
+		</foreach>
+	</block>
+</form>
+</vxml>
